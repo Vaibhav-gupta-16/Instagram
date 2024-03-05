ccf51faccec32a6c58004503fcee797f6b6f3c57
Vaibhav-gupta-16 <156050226+Vaibhav-gupta-16@users.noreply.github.com>
2024-03-06 01:46:47 +0530

    code

--- my-app/src/app/login/page.tsx
+++ my-app/src/app/login/page.tsx
@@ -1,6 +1,11 @@
 import Link from 'next/link'
 import React from 'react'
 import './login.css'
+import '@fortawesome/fontawesome-svg-core/styles'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCompass, faHome, faPersonDotsFromLine, faSearch } from '@fortawesome/free-solid-svg-icons'
+import '@fortawesome/free-brands-svg-icons'
+import { faFacebookMessenger, faRedRiver } from '@fortawesome/free-brands-svg-icons'
 export default function page() {
     return (
         <>
@@ -11,14 +16,14 @@ export default function page() {
                         <h1>Instagram</h1>
 
                         <ul>
-                            <li>Home</li>
-                            <li>Search</li>
-                            <li>Explore</li>
-                            <li>Reels</li>
-                            <li>Messages</li>
-                            <li>Notifications</li>
-                            <li>Create</li>
-                            <li>Profile</li>
+                            <li><FontAwesomeIcon className='mr-3' icon={faHome} size='1x' />  Home</li>
+                            <li><FontAwesomeIcon className='mr-3' icon={faSearch} size='1x' /> Search</li>
+                            <li><FontAwesomeIcon className='mr-3' icon={faCompass} size='1x' /> Explore</li>
+                            <li><FontAwesomeIcon className='mr-3' icon={faRedRiver} size='1x' /> Reels</li>
+                            <li><FontAwesomeIcon className='mr-3' icon={faFacebookMessenger} size='1x' /> Messages</li>
+                            <li><FontAwesomeIcon className='mr-3' icon={faPersonDotsFromLine} size='1x' /> Notifications</li>
+                            <li><FontAwesomeIcon className='mr-3' icon={faHome} size='1x' /> Create</li>
+                            <li><FontAwesomeIcon className='mr-3' icon={faHome} size='1x' /> Profile</li>
                         </ul>
                         <div>
                             <ul className='bf'>
