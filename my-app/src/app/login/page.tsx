afb62d70244cc1b15d981bf3da9beeb40ed27d41
Vaibhav-gupta-16 <156050226+Vaibhav-gupta-16@users.noreply.github.com>
2024-03-11 10:32:21 +0530

    code

--- my-app/src/app/login/page.tsx
+++ my-app/src/app/login/page.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 import './login.css'
 import '@fortawesome/fontawesome-svg-core/styles'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCompass, faHome, faPersonDotsFromLine, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faCompass, faHome, faPersonDotsFromLine, faSearch, faSquarePlus, faVideo } from '@fortawesome/free-solid-svg-icons'
 import '@fortawesome/free-brands-svg-icons'
 import { faFacebookMessenger, faRedRiver } from '@fortawesome/free-brands-svg-icons'
+import { faHeart } from '@fortawesome/free-solid-svg-icons/faHeart'
+import { faClapperboard } from '@fortawesome/free-solid-svg-icons/faClapperboard'
 export default function page() {
     return (
         <>
@@ -19,19 +21,57 @@ export default function page() {
                             <li><FontAwesomeIcon className='mr-3' icon={faHome} size='1x' />  Home</li>
                             <li><FontAwesomeIcon className='mr-3' icon={faSearch} size='1x' /> Search</li>
                             <li><FontAwesomeIcon className='mr-3' icon={faCompass} size='1x' /> Explore</li>
-                            <li><FontAwesomeIcon className='mr-3' icon={faRedRiver} size='1x' /> Reels</li>
+                            <li><FontAwesomeIcon className='mr-3' icon={faClapperboard} size='1x' /> Reels</li>
                             <li><FontAwesomeIcon className='mr-3' icon={faFacebookMessenger} size='1x' /> Messages</li>
-                            <li><FontAwesomeIcon className='mr-3' icon={faPersonDotsFromLine} size='1x' /> Notifications</li>
-                            <li><FontAwesomeIcon className='mr-3' icon={faHome} size='1x' /> Create</li>
-                            <li><FontAwesomeIcon className='mr-3' icon={faHome} size='1x' /> Profile</li>
+                            <li><FontAwesomeIcon className='mr-3' icon={faHeart} size='1x' /> Notifications</li>
+                            <li><FontAwesomeIcon className='mr-3' icon={faSquarePlus} size='1x' /> Create</li>
+                            <li><span id='p'></span> Profile</li>
                         </ul>
                         <div>
                             <ul className='bf'>
-                                <li>Threads</li>
-                                <li>More</li>
+                                <li> @ Threads</li>
+                                <li> <FontAwesomeIcon className='mr-3' icon={faBars} size='1x' />  More</li>
                             </ul>
                         </div>
                     </div>
+
+                    <div className="col-6 b">
+                        <div className="container-fluid cf2">
+                            <div className="row r2 flex carousel slide">
+                                <div className="carousel-inner">
+                                    <div className='d1 carousel-item active'></div>
+                                    <div className='d1 carousel-item'></div>
+                                    <div className='d1 carousel-item'></div>
+                                    <div className='d1 carousel-item'></div>
+                                    <div className='d1 carousel-item'></div>
+                                    <div className='d1 carousel-item'></div>
+                                    <div className='d1 carousel-item'></div>
+                                    <div className='d1 carousel-item'></div>
+                                    <div className='d1 carousel-item'></div>
+                                    <div className='d1 carousel-item'></div>
+                                    <div className='d1 carousel-item'></div>
+                                    <div className='d1 carousel-item'></div>
+                                    <div className='d1 carousel-item'></div>
+                                </div>
+
+                                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span className="visually-hidden">Previous</span>
+                                </button>
+                                <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span className="visually-hidden">Next</span>
+                                </button>
+                            </div>
+
+                            <div className="row r3"></div>
+                        </div>
+                    </div>
+
+                    <div className="col-4 c">
+
+                    </div>
+
                 </div>
             </div>
 
